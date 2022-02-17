8dd358d687723915d6d445876e216908f19b8a89
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-02-17 12:20:15 +0100

    scaffold invoice controller

---
+++ app/Http/Controllers/InvoiceController/InvoiceController.php.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\InvoiceController;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class InvoiceController.php extends Controller
+{
+    //
+}
