bf1fbfb248c063054d7c7c6455fd91c637957bfe
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-02-17 12:23:17 +0100

    modify controller file

--- app/Http/Controllers/InvoiceController/InvoiceController.php
+++ app/Http/Controllers/InvoiceController/InvoiceController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class InvoiceController.php extends Controller
+class InvoiceController extends Controller
 {
-    //
+    
 }
