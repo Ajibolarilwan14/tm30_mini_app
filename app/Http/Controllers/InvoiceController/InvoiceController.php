0e0c37629a4a5ccdcbec7fca23cb33c5dbe86079
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-02-17 12:35:21 +0100

    setup the 2 method in controller

--- app/Http/Controllers/InvoiceController/InvoiceController.php
+++ app/Http/Controllers/InvoiceController/InvoiceController.php
@@ -3,9 +3,40 @@
 namespace App\Http\Controllers\InvoiceController;
 
 use App\Http\Controllers\Controller;
+use App\Models\Invoice;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
-    
+    public function store(Request $request)
+    {
+        $request->validate([
+            'total' => 'required',
+            'amount' => 'required',
+            'tax' => 'required'
+        ]);
+
+        $amount = $request->amount;
+        $tax = $request->tax;
+
+        $total_after_tax = $amount * $tax;
+
+        $invoice = new Invoice();
+        $invoice->total = $total_after_tax;
+        $invoice->amount = $request->amount;
+        $invoice->tax = $request->tax;
+
+        $invoice->save();
+
+        return redirect()->back();
+
+    }
+
+    public function all_invoices()
+    {
+        $invoice = Invoice::latest();
+
+        dd($invoice);
+    }
 }
